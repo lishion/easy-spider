30179f43a752ef9b38e69e70ba840c5409c0d128
linex <544670411@qq.com>
2020-03-16 22:06:43 +0800

    新增: 版本信息

--- easy_spider/__init__.py
+++ easy_spider/__init__.py
@@ -2,3 +2,5 @@ from easy_spider.core.env import async_env
 from easy_spider.core.spider import AsyncSpider
 from easy_spider.network.request import Request
 from easy_spider.network.response import Response, HTMLResponse, TextResponse
+
+__version__ = "1.0.0"
