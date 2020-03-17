7aca621033549d6421daba747736f74609e00a72
linex <544670411@qq.com>
2020-03-17 14:28:50 +0800

    新增: pypi打包支持

--- easy_spider/__init__.py
+++ easy_spider/__init__.py
@@ -3,4 +3,5 @@ from easy_spider.core.spider import AsyncSpider
 from easy_spider.network.request import Request
 from easy_spider.network.response import Response, HTMLResponse, TextResponse
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
+name = "easy_spider"
