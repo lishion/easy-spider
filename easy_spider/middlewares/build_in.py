099c859fa2d5eaceca7554836b0c9398fa3de4ab
linex <544670411@qq.com>
2020-04-01 09:50:11 +0800

    新增: ChainMiddleware 新增 extend 方法便于扩展

--- easy_spider/middlewares/build_in.py
+++ easy_spider/middlewares/build_in.py
@@ -2,21 +2,21 @@ from abc import ABC, abstractmethod
 from easy_spider.network.request import Request
 from easy_spider.network.response import Response
 from easy_spider.tool import get_public_attr, copy_attr
-from typing import Tuple, Iterable
+from typing import List, Iterable, Optional
 from easy_spider.filters.build_in import Filter
 
 
 class RequestMiddleware(ABC):
 
     @abstractmethod
-    def transform(self, requests: Iterable[Request], response: Response) -> Iterable[Request]: pass
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]: pass
 
 
 class FilterMiddleware(RequestMiddleware):
     def __init__(self, filter: Filter):
         self._filter = filter
 
-    def transform(self, requests: Iterable[Request], response: Response) -> Iterable[Request]:
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]:
         yield from filter(self._filter.accept, requests)
 
 
@@ -24,7 +24,7 @@ class ExtractorFilterMiddleware(FilterMiddleware):
     def __init__(self, filter: Filter):
         super().__init__(filter)
 
-    def transform(self, requests: Iterable[Request], response: Response) -> Iterable[Request]:
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]:
         if not response:  # 如果 response 为空，则说明为初始 requests，不需要进行过滤
             yield from requests
         else:
@@ -33,7 +33,7 @@ class ExtractorFilterMiddleware(FilterMiddleware):
 
 class GenerationMiddleware(RequestMiddleware):
 
-    def transform(self, requests: Iterable[Request], response) -> Iterable[Request]:
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]:
         if response:
             source_request = response.request
             for request in requests:
@@ -49,7 +49,7 @@ class SetAttrMiddleware(RequestMiddleware):
         self._from_instance = from_instance
         self._attrs = attrs
 
-    def transform(self, requests: Iterable[Request], response: Response) -> Iterable[Request]:
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]:
         for request in requests:
             for attr in (self._attrs or get_public_attr(request)):  # 如果有指定的 attr 则使用指定的 attr, 否则动态获取
                 hasattr(self._from_instance, attr) and copy_attr(attr, self._from_instance, request)
@@ -59,10 +59,13 @@ class SetAttrMiddleware(RequestMiddleware):
 class ChainMiddleware(RequestMiddleware):
 
     def __init__(self, *middlewares: RequestMiddleware):
-        self._middlewares: Tuple[RequestMiddleware] = middlewares
+        self._middlewares: List[RequestMiddleware] = list(middlewares)
 
-    def transform(self, requests: Iterable[Request], response: Response) -> Iterable[Request]:
+    def transform(self, requests: Iterable[Request], response: Optional[Response]) -> Iterable[Request]:
         r = requests
         for middleware in self._middlewares:
             r = middleware.transform(r, response)
         yield from r
+
+    def extend(self, middlewares):
+        self._middlewares.extend(middlewares)
